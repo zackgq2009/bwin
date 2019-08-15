b0900ce0b9bd69164a7b20864b3c39e5a57e59ed
johnnyKuo <zackgq@hotmail.com>
2019-08-15 23:29:28 +0800

    Add datasource.properties and update the ConnectMysql

--- src/main/java/util/ConnectMysql.java
+++ src/main/java/util/ConnectMysql.java
@@ -1,4 +1,26 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectMysql {
+
+    private static String HOSTNAME;
+    private static String DATABASE;
+    private static String TABLE;
+
+    public ConnectMysql() {
+    }
+
+    public static void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_demo?useSSL=false&serverTimezone=UTC","root","password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("异常造成数据库连接失败");
+        } finally {
+
+        }
+    }
 }
