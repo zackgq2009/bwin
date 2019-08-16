2a45576f3ab7033bddd633b9c0b14b97d574162f
zackgq2009 <zackgq@hotmail.com>
2019-08-16 18:20:34 +0800

    finish connectMysql util

--- src/main/java/service/MatchRedNumber.java
+++ src/main/java/service/MatchRedNumber.java
@@ -1,4 +1,6 @@
 package service;
 
 public class MatchRedNumber {
+
+
 }
--- src/main/java/util/ConnectMysql.java
+++ src/main/java/util/ConnectMysql.java
@@ -1,26 +1,64 @@
 package util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectMysql {
 
-    private static String HOSTNAME;
-    private static String DATABASE;
-    private static String TABLE;
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
 
-    public ConnectMysql() {
-    }
+    private static BasicDataSource dataSource;
 
-    public static void connect() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_demo?useSSL=false&serverTimezone=UTC","root","password");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("异常造成数据库连接失败");
-        } finally {
+    private static Connection connection;
 
+    private static String URL = PropertiesUtil.getProperty("db.url", "jdbc:mysql://localhost:3306/mysql");
+    private static String USERNAME = PropertiesUtil.getProperty("db.username", "root");
+    private static String PASSWORD = PropertiesUtil.getProperty("db.password", "password");
+    private static String initialSize = PropertiesUtil.getProperty("db.initialSize", "20");
+    private static String maxActive = PropertiesUtil.getProperty("db.maxActive", "50");
+    private static String maxIdle = PropertiesUtil.getProperty("db.maxIdle", "20");
+    private static String minIdel = PropertiesUtil.getProperty("db.minIdle", "10");
+    private static String maxWait = PropertiesUtil.getProperty("db.maxWait", "10");
+    private static String defaultAutoCommit = PropertiesUtil.getProperty("db.defaultAutoCommit", "true");
+    private static String minEvictableIdelTimeMillis = PropertiesUtil.getProperty("db.minEvictableIdleTimeMillis", "3600000");
+
+
+    static{
+        if (dataSource == null)
+        {
+            BasicDataSource ds = new BasicDataSource();
+            ds.setUrl(URL);
+            ds.setUsername(USERNAME);
+            ds.setPassword(PASSWORD);
+
+
+            ds.setMinIdle(Integer.parseInt(minIdel));
+            ds.setMaxIdle(Integer.parseInt(maxIdle));
+            ds.setMaxOpenPreparedStatements(100);
+
+            dataSource = ds;
         }
     }
+
+    public static Connection getConnnection() {
+        if (dataSource == null) {
+            return null;
+        } else {
+            try {
+                connection = dataSource.getConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                logger.error("连接数据库有异常");
+            } finally {
+                return connection;
+            }
+        }
+    }
+
 }
